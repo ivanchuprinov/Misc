2d7997edc09cb40ecbbee4177c9c10cc09b6c9b5
ivanchuprinov <chuprii@wwu.edu>
2018-02-14 13:50:05 -0800

    eh

--- lab05.rkt
+++ lab05.rkt
@@ -8,16 +8,35 @@
 ;; W01334363
 ;;
 ;; The purpose of this program is
-;; to 
+;; to evaluate an expression
+;; (in a form of a list) in a context
+;; of an environment (in a form of
+;; a list of lists), also taking
+;; in account special forms of
+;; expressions (if, cond)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide lookup evaluate)
+
+;; Returns the size of a list
+(define list-size
+  (lambda (list)
+    (let loop ((ls list) (counter 0))
+      ;; Initiate a loop on the list
+      (if (empty? ls)
+          ;; When the expression is empty, we return the size of the list
+          counter
+          ;; Otherwise, increment the size counter
+          (begin
+            (set! counter (+ 1 counter))
+            ;; Loop is called again on a cdr portion of the list
+            (loop (cdr ls) counter))))))
 
 ;; Looks up a symbol in an environment (represented as a list of lists)
 ;; and returns its attached value from the environment.
 (define lookup
   (lambda (symbol environment)
     (if (empty? environment)
-        (error "Symbol not found:" (display symbol))
+        (error "Symbol not found")
         (if (symbol? symbol)
             ;; if the first parameter is a symbol
             (if (eq? (car (car environment)) symbol)
@@ -40,91 +59,72 @@
       ((symbol? expression) (lookup expression environment))
       ;; If the expression is a list:
       ((list? expression)
-       (display expression)
-       (newline)
        (if(special-form? expression)
-              (evaluate-special-form expression environment)
-              (if (procedure? (evaluate (car expression) environment))
-                  ;; If the first value of a list is a procedure:
-                  (let ((ls empty))
-                    ;; We make a local variable pointing to an empty list
-                    (let loop ((exp (cdr expression)))
-                      ;; Initiate a loop on a cdr portion of the expression
-                      (if (empty? exp)
-                          ;; When the expression is empty, we return the list, loop breaks
-                          ls
-                          ;; Otherwise, we evaluate the first element of the expression
-                          ;; and add it to the list
-                          (begin
-                            (set! ls (append ls (list (evaluate (car exp) environment))))
-                            ;; Loop is called again on a cdr portion of the expression
-                            (loop (cdr exp)))))
-                    ;; We apply a procedure stored in the first element of the expression
-                    ;; to the list created from the rest of the elements of the expression
-                    (apply (evaluate (car expression) environment) ls))
-                  ;; If the first thing in the evaluated list is not a procedure,
-                  ;; an error is returned.
-                  (error "First element is not a procedure"))))
+          ;; if the expression is in special form, evaluate the special form
+          (evaluate-special-form expression environment)
+          ;; otherwise, treat it as a normal expression
+          (if (procedure? (evaluate (car expression) environment))
+              ;; If the first value of a list is a procedure:
+              (let ((ls empty))
+                ;; We make a local variable pointing to an empty list
+                (let loop ((exp (cdr expression)))
+                  ;; Initiate a loop on a cdr portion of the expression
+                  (if (empty? exp)
+                      ;; When the expression is empty, we return the list, loop breaks
+                      ls
+                      ;; Otherwise, we evaluate the first element of the expression
+                      ;; and add it to the list
+                      (begin
+                        (set! ls (append ls (list (evaluate (car exp) environment))))
+                        ;; Loop is called again on a cdr portion of the expression
+                        (loop (cdr exp)))))
+                ;; We apply a procedure stored in the first element of the expression
+                ;; to the list created from the rest of the elements of the expression
+                (apply (evaluate (car expression) environment) ls))
+              ;; If the first thing in the evaluated list is not a procedure,
+              ;; an error is returned.
+              (error "First element is not a procedure"))))
       ;; If the expression is anything else, an error is returned.
       (else (error "Wrong expression type.")))))
 
 ;; Returns true if the list is in special form, otherwise, returns false.
 (define special-form?
   (lambda (expression)
-    (if (or (eq? (car expression) 'cond) (eq? (car expression) 'if))
+    (if (and (or (eq? (car expression) 'cond) (eq? (car expression) 'if)) (list? expression))
         #t
         #f)))
 
+;; Evaluates special form
 (define evaluate-special-form
   (lambda (expression environment)
     (if (not (special-form? expression))
+        ;; if the expression is not in special form, return error
         (error "Expression is not in special form.")
+        ;; otherwise, check, which special form it is, and evaluate accordingly
         (cond
           ((eq? (car expression) 'cond) (evaluate-cond (cdr expression) environment))
           ((eq? (car expression) 'if) (evaluate-if (cdr expression) environment))))))
 
+;; Evaluates if special form expression
 (define evaluate-if
   (lambda (expression environment)
-    (display expression)
-    (newline)
-    (if (evaluate (car expression) environment)
-        (evaluate (car (cdr expression)) environment)
-        (evaluate (car (cdr (cdr expression))) environment))))
+    ;; if there are exactly 3 elements after if, evaluate the statements
+    (if (= (list-size expression) 3)
+        ;; if the condition returns true
+        (if (evaluate (car expression) environment)
+            ;; evaluate the first expression
+            (evaluate (car (cdr expression)) environment)
+            ;; otherwise, evaluate the second expression
+            (evaluate (car (cdr (cdr expression))) environment))
+        ;; if there are more or less than 3 elements after if, the syntax is bad
+        (error "Bad syntax. Expected 3 things after the if-statement"))))
 
+;; NOTE: Checks if there's no else  or else is not the last statement. throw some errors
+;; Evaluates a cond special form expression
 (define evaluate-cond
   (lambda (expression environment)
-    (if (eq? (caar expression) 'else)
+    (if (evaluate (caar expression) environment)
         (evaluate (cadar expression) environment)
-        (if (evaluate (caar expression) environment)
-            (evaluate (cadar expression) environment)
-            (evaluate-cond (cdr expression) environment)))))
-
-
-(define env (list
-             (list 'x 5)
-             (list '+ +)
-             (list '* *)
-             (list '- -)
-             (list '= =)))
-
-(special-form? '(if (= 1 1) (- (+ 1 1) 5) (- 2 2)))
-;;(evaluate '(if (= 3 (+ 1 1)) (- (+ 1 2) 5) (- 2 2)) env)
-(evaluate '(cond ((= 1 2) (+ 1 2))
-                 ((= 1 1) (+ 1 3))
-                 (else (+ 1 5))) env)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        (evaluate-cond (cdr expression) environment))))
 
 
