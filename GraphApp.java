958232315931e7292e93292201283ffe534cb098
ivanchuprinov <chuprii@wwu.edu>
2018-03-16 11:12:04 -0700

    upd

--- GraphApp.java
+++ GraphApp.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
 public class GraphApp{
-	public static List<String> findIndex(String[][] tickets){
-		int airportCount = 0;
+	public static List<String> findItinerary(String[][] tickets){
+		int airportCount = 0;	// Number of airports in an itinerary
 		Map<Integer, String> APmapIS = new HashMap<>();	// Airport = key, Index = value
 		Map<String, Integer> APmapSI = new HashMap<>();	// Index = key, Airport = value
+		// Fill hashmaps
 		for(int i = 0; i<tickets.length; i++){
 			if(!APmapIS.containsValue(tickets[i][0])){
 				APmapIS.put(airportCount, tickets[i][0]);
@@ -18,44 +19,42 @@ public class GraphApp{
 			}
 		}
 	
-	
+		// Graph is a set of verticies and set of edges
 		List<Vertex> graph = new ArrayList <Vertex>();
 		
+		// Create verticies
 		for(int i = 0; i<airportCount; i++){
 			graph.add(new Vertex(APmapIS.get(i)));
 		}
+		
 		// Create edges
 		for(int i = 0; i<tickets.length; i++){
 			graph.get(APmapSI.get(tickets[i][0])).neighbors.add(graph.get(APmapSI.get(tickets[i][1])));
 		}
 		
-		Queue<String> q = new LinkedList<String>();
-		q.add("JFK");
+		// Stack to store values
+		Stack<String> st = new LinkedList<String>();
+		// Visit the JFK vertex and everything it points to
 		for(int i = 0; i<graph.size(); i++){
 			Vertex v = graph.get(i);
-			if(v.visit.equals("false"))
-				visit(v, q, false);
+			if(v.value.equals("JFK"))
+				visit(v, st);
 		}
 		
 		List<String> result = new ArrayList<String>();
-		while(!q.isEmpty())
-			result.add(q.remove());
+		while(!st.isEmpty())
+			result.add(st.pop());
 		return result;
 	}
 	
-	private static void visit(Vertex vertex, Queue<String> q, boolean JFKfound){
-		if(!vertex.equals("false"))
+	private static void visit(Vertex v, Stack<String> st){
+		if(!v.visit.equals("false"))
 			return;
-		if(!JFKfound && !vertex.value.equals("JFK"))
-			for(int i = 0; i<vertex.neighbors.size(); i++)
-				visit(vertex.neighbors.get(i), q, JFKfound);
-		else
-			JFKfound = true;
-		vertex.visit = "temporary";
-		for(int i = 0; i<vertex.neighbors.size(); i++)
-			visit(vertex.neighbors.get(i), q, JFKfound);
-		vertex.visit = "permanent";
-		q.add(vertex.value);
+		v.visit = "temporary";
+		for(int i = 0; i<v.neighbors.size(); i++)
+			visit(v.neighbors.get(i), st);
+		v.visit = "permanent";
+		st.push(v.value);
 	}
 }
 
